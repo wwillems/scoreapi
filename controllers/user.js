c02e1322562707257e044033f2f59886ccdc31c6
William Willems <william.willems@gmail.com>
2015-01-24 14:23:41 +0100

    delete user

--- controllers/user.js
+++ controllers/user.js
@@ -30,7 +30,7 @@ exports.getUsers = function(req, res) {
 // Create endpoint /api/users/:user_id for DELETE
 exports.deleteUser = function(req, res) {
   // Use the User model to find a specific score and remove it
-  User.findByIdAndRemove(req.params.score_id, function(err) {
+  User.findByIdAndRemove(req.params.user_id, function(err) {
     if (err)
       res.send(err);
 
